678de6150a451ba6222dda168ad6d04a8d39ebd2
Hosam <hosamabd57@gmail.com>
2025-05-06 00:36:20 +0300

    Add files via upload

---
+++ be-secript.js
@@ -0,0 +1,106 @@
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning') 
+
+let timeRunning = 3000 
+let timeAutoNext = 7000
+
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+let runTimeOut 
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight /* trigger reflow */
+    runningTime.style.animation = null 
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+
+    clearTimeout(runTimeOut)
+
+    runTimeOut = setTimeout( () => {
+        carousel.classList.remove('next')
+        carousel.classList.remove('prev')
+    }, timeRunning)
+
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation() // Reset the running time animation
+}
+
+// Start the initial animation 
+resetTimeAnimation()
+
+//**** script / login ****//
+
+
+//**** script 1 ****//
+function myMenuFunction() {
+    var i = document.getElementById("navMenu");
+
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
+
+   //**** script 2 ****//
+   var a = document.getElementById("loginBtn");
+   var b = document.getElementById("registerBtn");
+   var x = document.getElementById("login");
+   var y = document.getElementById("register");
+
+   function login() {
+       x.style.left = "4px";
+       y.style.right = "-520px";
+       a.className += " white-btn";
+       b.className = "btn";
+       x.style.opacity = 1;
+       y.style.opacity = 0;
+   }
+
+   function register() {
+       x.style.left = "-510px";
+       y.style.right = "5px";
+       a.className = "btn";
+       b.className += " white-btn";
+       x.style.opacity = 0;
+       y.style.opacity = 1;
+   }
+
+
+   /**********************/
+
+
+
+
